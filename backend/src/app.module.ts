1cbc0eb1c96665ed6d8093fb19b05a5624f8378f
Anthony Leotta <tony@leotta.com>
2022-04-18 19:36:58 -0400

    adds news endpoint

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CurrentController } from './current/current.controller';
 import { PagePropsService } from './page-props/page-props.service';
+import { NewsService } from './news/news.service';
+import { NewsController } from './news/news.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, CurrentController],
-  providers: [AppService, PagePropsService]
+  controllers: [AppController, CurrentController, NewsController],
+  providers: [AppService, PagePropsService, NewsService]
 })
 export class AppModule {}
